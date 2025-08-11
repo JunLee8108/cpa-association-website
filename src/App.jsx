8ce855607b73c4e6997f6b9ea513bda299c7716b
JunLee8108 <lejhn1@gmail.com>
2025-08-11 15:06:53 -0500

    quick fix v5

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router";
+
 import Home from "./components/pages/home/Home";
+import Navbar from "./components/layout/Navbar/Navbar";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
